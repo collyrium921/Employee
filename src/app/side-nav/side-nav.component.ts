847801e1ef99d61ab97f931f5a9ff1642a927472
Kajal Singhal <kajusinghal921@gmail.com>
2022-08-29 00:23:07 +0530

    changes to json

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -8,14 +8,22 @@ import { Router } from '@angular/router';
 })
 export class SideNavComponent implements OnInit {
   isListCompany:boolean=true
-  constructor(private router:Router) { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
+    if (this.router.url.includes('add')) {
+      console.log(true)
+      this.isListCompany = false
+    }
+    else {
+      this.isListCompany=true
+    }
   }
 
   goToAddEmployee() {
     this.isListCompany=false
-    this.router.navigateByUrl('add')
+    this.router.navigateByUrl('/add')
   }
   goToListEmployee() {
     this.isListCompany=true
