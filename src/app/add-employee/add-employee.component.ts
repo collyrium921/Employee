97929b8bcf18289ae533b54cc643ea025f57c009
Kajal Singhal <kajusinghal921@gmail.com>
2022-08-29 00:37:02 +0530

    comments added

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -13,6 +13,7 @@ import { ErrorStateMatcher } from '@angular/material/core'
 import { DESIGNATION_LIST } from 'src/constants/designation'
 import { AppService } from '../app.service'
 import { Router } from '@angular/router'
+import { MatSnackBar } from '@angular/material/snack-bar'
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(
@@ -34,81 +35,103 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./add-employee.component.scss'],
 })
 export class AddEmployeeComponent implements OnInit {
-  skills = SKILLS;
-  designationList= DESIGNATION_LIST;
+  //variable declaration
+  skills = SKILLS
+  designationList = DESIGNATION_LIST
   public companyForm!: FormGroup
   matcher = new MyErrorStateMatcher()
-  constructor(private fb: FormBuilder, private appService: AppService,
-  private router: Router) {
-    
-  }
+
+  constructor(
+    private fb: FormBuilder,
+    private appService: AppService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+  ) {}
+
   ngOnInit(): void {
+    //define the fields of form group
     this.companyForm = this.fb.group({
-      id:'',
+      id: '',
       companyName: ['', [Validators.required, Validators.maxLength(50)]],
       address: [''],
       email: ['', [Validators.required, Validators.maxLength(100)]],
       phoneNumber: ['', [Validators.required, Validators.maxLength(15)]],
       empInfo: this.fb.array([]),
     })
-    this.addEmployee();
+    this.addEmployee()
   }
 
+  //get employee controls as formArray
   empInfo(): FormArray {
     return this.companyForm.get('empInfo') as FormArray
   }
 
+  //new employee data
   newEmployee(): FormGroup {
     return this.fb.group({
       empName: ['', [Validators.required, Validators.maxLength(25)]],
       designation: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.maxLength(100)]],
-      phoneNumber:['', [Validators.required, Validators.maxLength(15)]],
+      phoneNumber: ['', [Validators.required, Validators.maxLength(15)]],
       skillInfo: this.fb.array([]),
       educationInfo: this.fb.array([]),
     })
   }
 
+  //add employee to the company form in empInfo array
   addEmployee() {
     this.empInfo().push(this.newEmployee())
   }
 
+  //get skills as formArray
   employeeSkills(empIndex: number): FormArray {
-    return this.empInfo().at(0).get('skillInfo') as FormArray
+    return this.empInfo().at(empIndex).get('skillInfo') as FormArray
   }
 
+  //new skill information data
   newSkill(): FormGroup {
     return this.fb.group({
       skillName: ['', [Validators.required]],
       skillRating: ['', [Validators.required]],
-    });
+    })
   }
 
+  //get employee information as formArray
   employeeEducation(empIndex: number): FormArray {
     return this.empInfo().at(0).get('educationInfo') as FormArray
   }
 
+  //new employee information data
   newEductation(): FormGroup {
     return this.fb.group({
       instituteName: ['', [Validators.required, Validators.maxLength(50)]],
       courseName: ['', [Validators.required, Validators.maxLength(25)]],
       completedYear: ['', [Validators.required]],
-    });
+    })
   }
 
+  //add employee skill to the array
   addEmployeeSkill(empIndex: number) {
-    this.employeeSkills(0).push(this.newSkill())
+    this.employeeSkills(empIndex).push(this.newSkill())
   }
 
+  // add employee education to array
   addEmployeeEducation(empIndex: number) {
-    this.employeeEducation(0).push(this.newEductation())
+    this.employeeEducation(empIndex).push(this.newEductation())
   }
 
+  //add company data to the local json file
   addCompany() {
     this.companyForm.value.id = new Date().getTime()
     console.log(this.companyForm.value)
-    this.appService.addCompany(this.companyForm.value).subscribe(res => {
-        this.router.navigateByUrl('')
+    this.appService.addCompany(this.companyForm.value).subscribe((res) => {
+      this.snackBar.open('Delete successful', 'Close', {
+        duration: 1000,
+        panelClass: ['snack-notification'],
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+      })
+      this.router.navigateByUrl('')
     })
   }
 
--- src/app/list-employee/list-employee.component.ts
+++ src/app/list-employee/list-employee.component.ts
@@ -1,53 +1,59 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core'
 import { MatPaginator } from '@angular/material/paginator'
-import employeeList from '../employeeList.json'
 import { Inject } from '@angular/core'
 import {
   MatDialogRef,
   MatDialog,
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog'
-import { HttpClient } from '@angular/common/http'
 import { AppService } from '../app.service'
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { LiveAnnouncer } from '@angular/cdk/a11y'
 import { MatSort, Sort } from '@angular/material/sort'
+import { IEmployee } from '../interfaces/employee'
 @Component({
   selector: 'app-list-employee',
   templateUrl: './list-employee.component.html',
   styleUrls: ['./list-employee.component.scss'],
 })
 export class ListEmployeeComponent implements AfterViewInit {
+  //variable declaration
   displayedColumns: string[] = [
     'Company Name',
     'Email',
     'Phone Number',
     'Action',
   ]
-  employee: any
+  employee: IEmployee[] = [];
+
+
   constructor(
     public dialog: MatDialog,
     private appService: AppService,
     private snackBar: MatSnackBar,
-    private liveAnnouncer: LiveAnnouncer
+    private liveAnnouncer: LiveAnnouncer,
   ) {
+    //show list of companies on the screen initally
     this.listCompanies()
   }
   @ViewChild(MatPaginator) paginator!: MatPaginator
-  @ViewChild(MatSort) sort!: MatSort;
-  
+  @ViewChild(MatSort) sort!: MatSort
+
   ngAfterViewInit() {
-    this.employee.paginator = this.paginator
-    this.employee.sort = this.sort;
+    // this.employee.paginator = this.paginator
+    // this.employee.sort = this.sort;
   }
 
+  //function to call list of Companies
   listCompanies() {
     this.appService.getList().subscribe((data) => {
       this.employee = data
     })
   }
 
-  openDialog(i: number, emp: any): void {
+
+  //function to show confirmation dialog and delete the company on confirmation
+  deleteCompany(emp: IEmployee) {
     const dialogRef = this.dialog.open(ConfirmationDialogue, {})
     dialogRef.afterClosed().subscribe((confirmed: boolean) => {
       if (confirmed) {
@@ -64,15 +70,18 @@ export class ListEmployeeComponent implements AfterViewInit {
     })
   }
 
+  //function to sort the data in the table
   announceSortChange(sortState: Sort) {
     if (sortState.direction) {
-      this.liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+      this.liveAnnouncer.announce(`Sorted ${sortState.direction}ending`)
     } else {
-      this.liveAnnouncer.announce('Sorting cleared');
+      this.liveAnnouncer.announce('Sorting cleared')
     }
   }
 }
 
+
+// delete confirmation box html
 @Component({
   selector: 'confirmation-dialog',
   templateUrl: 'confirmation-dialogue.html',
